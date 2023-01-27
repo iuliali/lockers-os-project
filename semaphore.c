ece16673708565a33c22ece55e6300fcf3842711
Iulia-Georgiana Talpalariu <92271141+iuliali@users.noreply.github.com>
2023-01-27 19:57:44 +0200

    fixes semaphore

--- semaphore.c
+++ semaphore.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdatomic.h>
+#include <Windows.h>
+
 
 typedef struct spinlock_mutex{
     volatile atomic_flag taken;
@@ -26,6 +28,8 @@ typedef struct semaphore{
 
 //const semaphore sem_init = {0};
 
+
+
 void init_sem(semaphore* sem, unsigned int value) {
     atomic_int val;
     atomic_store(&val, value);
@@ -34,53 +38,69 @@ void init_sem(semaphore* sem, unsigned int value) {
 }
 
 void wait(semaphore* sem){
-    printf("wait\n");
-    while(atomic_load(&sem->S) <= 0) ;
+//    atomic_int zero;
+//    atomic_store(&zero,0);
+    acquire(&sem->mutex);
+    //printf("thread id = %d : wait, sem : %d\n", pthread_self(), atomic_load(&sem->S));
+    //printf("wait : %d\n", sem->S);
+    while(atomic_load(&sem->S) < 1) ;
+    //printf("wait\n");
+    printf("thread %d entered sem: %d\n", pthread_self(), atomic_load(&sem->S));
+
+
     atomic_fetch_sub(&sem->S, 1);
+    release(&sem->mutex);
 }
 
 void post(semaphore* sem) {
     atomic_fetch_add(&sem->S, 1);
-    printf("post\n");
+    printf("thread %d out sem: %d\n", pthread_self(), atomic_load(&sem->S));
+
+    //printf("post\n");
 }
 
 
 
-int loop = 10;
+int loop = 100000;
 int n =0; int k = 0;
 semaphore s;
 
 
 void* foo(void *p)
 {
-    int i = 0;
-    for(i = 0; i < loop; i++) {
 
+
+//    for(i = 0; i < loop; i++) {
+        //printf("thread %d\n", *ptr);
         wait(&s);
-        ++n;
-        printf("%d\n", n);
-        post(&s);
+        //++n;
+        Sleep(3000);
+        //printf("%d\n", n);
 
-        //neprotejat de semafor
-        ++k;
-    }
+        post(&s);
+        //printf("thread %d out\n", pthread_self());
+
+
+//        //neprotejat de semafor
+//        ++k;
+//    }
 
     return NULL;
  }
 
-int count = 100;
+int count = 10;
 
 int main(int argc, char **argv)
 {
 
-    init_sem(&s, 1);
+    init_sem(&s, 3);
     int i;
     pthread_t pids[count];
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
     for(i = 0; i < count; i++) {
-        pthread_create(&pids[i], &attr, foo, NULL);
+        pthread_create(&pids[i], &attr, foo, i);
     }
 
     for(i = 0; i < count; i++) {
